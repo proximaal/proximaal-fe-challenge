1c12ee9925e793ba6743ca053e00b024b9751d3d
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-12 20:14:44 +0200

    Request school data

--- src/views/SchoolCardsView.vue
+++ src/views/SchoolCardsView.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { fetchData } from '../components/composables/fetchData.js'
+
+const { data, loading }  = fetchData('http://localhost:3000/school')
+</script>
+
 <template>
   <h1>School Cards</h1>
-</template>>
+<section v-if="loading">
+  <p>Loading...</p>
+</section>
+<section v-else-if="data">Data loaded</section>
+<section v-else>Error</section>
+</template>
