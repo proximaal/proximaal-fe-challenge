c233de37f65edd170257ec4530c213b2f902217d
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-12 15:06:50 +0200

    Create register user view

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,20 @@
+<script setup>
+import { reactive } from 'vue'
+
+const state = reactive({
+  username: '',
+  password: ''
+})
+</script>
+
 <template>
   <div>
     <h1>Register</h1>
+    <label for="username">Username</label>
+    <input  name="username" v-model="state.username" type="text">
+    <label for="password">Password</label>
+    <input name="password" v-model="state.password" type="password">
+    <button>Register</button>
   </div>
 </template>
 
