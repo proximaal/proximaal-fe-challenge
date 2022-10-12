1b3b55e0f35eae979475b223a53342d039470763
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-12 16:05:10 +0200

    Add login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive } from 'vue'
+
+const state = reactive({
+  username: '',
+  password: ''
+})
+</script>
+
+<template>
+  <h1>Login</h1>
+  <label for="username">Username</label>
+  <input  name="username" v-model="state.username" type="text">
+  <label for="password">Password</label>
+  <input name="password" v-model="state.password" type="password">
+  <button>Login</button>
+</template>
