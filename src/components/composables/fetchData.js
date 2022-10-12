ca40703444a717a1a864312f30550f7981675b80
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-12 20:12:55 +0200

    Create composable to fetch data

---
+++ src/components/composables/fetchData.js
@@ -0,0 +1,22 @@
+import { ref } from 'vue'
+import axios from 'axios'
+
+export function fetchData(url) {
+  const data = ref(null)
+  const error = ref(null)
+  const loading = ref(null)
+
+  axios.get(url)
+    .then((response) => {
+      console.log(response.data)
+      data.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+      error.value = error
+    })
+    .finally(() => { loading.value = false })
+    
+
+  return { data, error, loading }
+}
