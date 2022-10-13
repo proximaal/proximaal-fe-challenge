2d3612661cee7949b3653edda6e9101be15a5f64
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-13 17:40:28 +0200

    Account for consecutive select actions

--- src/components/SchoolCards/SearchSchools.vue
+++ src/components/SchoolCards/SearchSchools.vue
@@ -9,18 +9,21 @@ defineEmits(['searchSelection', 'currentQuery'])
 const query = ref('')
 const searchedCards = computed(() => {
   return props.schools
-    .filter(school => school.name.toLowerCase()
-    .includes(query.value.toLowerCase()))
+  .filter(school => school.name.toLowerCase()
+  .includes(query.value.toLowerCase()))
+  
 })
 
 </script>
 
 <template>
-  <label for="search">Search by name</label>
-  <input type="text" placeholder="Search" v-model="query">
-  <ul v-if="query !== ''">
-    <li v-for="card in searchedCards" :key="card.name">
-     <p @click="$emit('searchSelection', card.name)"> {{ card.name }}</p>
-    </li>
-  </ul>
+  <div>
+    <label for="search">Search by name</label>
+    <input type="text" placeholder="Search" v-model="query">
+    <ul v-if="query !== ''">
+      <li v-for="card in searchedCards" :key="card.name">
+        <p @click="$emit('searchSelection', card.name)"> {{ card.name }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
--- src/views/SchoolCardsView.vue
+++ src/views/SchoolCardsView.vue
@@ -4,17 +4,32 @@ import SchoolCard from '../components/SchoolCards/SchoolCard.vue'
 import SearchSchools from "../components/SchoolCards/SearchSchools.vue"
 import FilterSchools from "../components/SchoolCards/FilterSchools.vue"
 
-const { data, loading }  = fetchData('http://localhost:3000/school')
+const { data, allData, loading }  = fetchData('http://localhost:3000/school')
 
 const sortCards = (name) => {
   const searchedSchools = data.value.filter(school => school.name === name)
   data.value = searchedSchools
 }
 
+const cityChoices = ['Amsterdam', 'Rotterdam', 'Haarlem', 'Den Haag']
+const stateChoices = ['Zuid-Holland', 'Noord-Holland']
+const statusChoices = ['active', 'pending']
+
 const filterCards = (choice) => {
-  console.log(choice)
-  const searchedSchools = data.value.filter(school => school.city === choice)
-  data.value = searchedSchools
+  // Account for multiple selects
+  data.value = allData[0]
+  if ( cityChoices.includes(choice)) {
+    const searchedSchools = data.value.filter(school => school.city === choice)
+    data.value = searchedSchools
+  } else if (stateChoices.includes(choice)) {
+    const searchedSchools = data.value.filter(school => school.state === choice)
+    data.value = searchedSchools
+  } else if (statusChoices.includes(choice)) {
+    const searchedSchools = data.value.filter(school => school.status === choice)
+    data.value = searchedSchools
+  } else {
+    data.value = allData[0]
+  } 
 }
 
 </script>
@@ -27,12 +42,12 @@ const filterCards = (choice) => {
   <section v-else-if="data">
     <FilterSchools 
       type="city" 
-      :choices="['Amsterdam', 'Rotterdam', 'Haarlem', 'Den Haag']"
+      :choices="cityChoices"
       @filter-selection="filterCards"
       />
-    <FilterSchools @filter-selection="filterCards" type="status" :choices="['active', 'pending']"/>
-    <FilterSchools @filter-selection="filterCards" type="state" :choices="['Zuid-Holland', 'Noord-Holland']"/>
-    <SearchSchools @search-selection="sortCards" :schools="data" />
+    <FilterSchools @filter-selection="filterCards" type="status" :choices="statusChoices"/>
+    <FilterSchools @filter-selection="filterCards" type="state" :choices="stateChoices"/>
+    <SearchSchools @search-selection="sortCards" :schools="data"/>
     <ul>
       <SchoolCard 
         v-for="school in data" 
