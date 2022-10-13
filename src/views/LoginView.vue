779d873f92708f5b53c73e05f6ea8d38ffeeef2c
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-13 16:18:25 +0200

    Add form validation login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { user } from '@/stores/user'
 import { NButton } from 'naive-ui'
 
@@ -7,24 +7,30 @@ const state = reactive({
   username: '',
   password: ''
 })
+const required = ref(false)
 
 const store = user()
 
 const login = (payload) => {
-  payload = {
+  if (state.username === '' || state.password === '') {
+    required.value = true
+  } else {
+    payload = {
     username: state.username,
     password: state.password
   }
-  store.loginUser(payload)
+    store.loginUser(payload)
+  }
 }
 </script>
 
 <template>
   <h1>Login</h1>
   <label for="username">Username</label>
-  <input  name="username" v-model="state.username" type="text">
+  <input name="username" v-model="state.username" type="text">
   <label for="password">Password</label>
   <input name="password" v-model="state.password" type="password">
+  <p v-if="required">Username and Password are required fields</p>
   <n-button @click="login">login</n-button>
   <section v-if="store.userError">error</section>
   <section v-if="store.userLoading">loading...</section>
