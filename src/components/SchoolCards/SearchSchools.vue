91074683b37761ecf68d17774eed22cd286b4d9d
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-13 10:38:13 +0200

    Search for schools with auto complete

---
+++ src/components/SchoolCards/SearchSchools.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const props = defineProps({
+  schools: Array
+})
+const query = ref('')
+const searchedCards = computed(() => {
+  return props.schools
+    .filter(school => school.name.toLowerCase()
+    .includes(query.value.toLowerCase()))
+})
+
+</script>
+
+<template>
+  <label for="search">Search by name</label>
+  <input type="text" placeholder="Search" v-model="query">
+  <ul v-if="query !== ''">
+    <li v-for="card in searchedCards" :key="card.name">
+     {{ card.name }}
+    </li>
+  </ul>
+</template>
--- src/views/SchoolCardsView.vue
+++ src/views/SchoolCardsView.vue
@@ -1,23 +1,25 @@
 <script setup>
 import { fetchData } from '../components/composables/fetchData.js'
 import SchoolCard from '../components/SchoolCards/SchoolCard.vue'
+import SearchSchools from "../components/SchoolCards/SearchSchools.vue"
 
 const { data, loading }  = fetchData('http://localhost:3000/school')
 </script>
 
 <template>
   <h1>School Cards</h1>
-<section v-if="loading">
-  <p>Loading...</p>
-</section>
-<section v-else-if="data">
-  <ul>
-    <SchoolCard 
-      v-for="school in data" 
-      :school="school" 
-      :key="school.id" 
-      />
-  </ul>
-</section>
-<section v-else>Error</section>
+  <section v-if="loading">
+    <p>Loading...</p>
+  </section>
+  <section v-else-if="data">
+    <SearchSchools :schools="data" />
+    <ul>
+      <SchoolCard 
+        v-for="school in data" 
+        :school="school" 
+        :key="school.id" 
+        />
+    </ul>
+  </section>
+  <section v-else>Error</section>
 </template>
